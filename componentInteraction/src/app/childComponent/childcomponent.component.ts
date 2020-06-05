564f695b2e2d03a9145b968ae15a9baca40dabde
venkatesh1252 <venky.kandur@gmail.com>
2020-06-04 22:53:42 -0400

    default files

---
+++ componentInteraction/src/app/childComponent/childcomponent.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-childcomponent',
+  templateUrl: './childcomponent.component.html',
+  styleUrls: ['./childcomponent.component.scss']
+})
+export class ChildcomponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
