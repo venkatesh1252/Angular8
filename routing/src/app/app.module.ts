6755f4074ebab311652fdca2015bf34b1f83b46b
venkatesh1252 <venky.kandur@gmail.com>
2020-06-10 17:11:03 -0400

    added components

--- routing/src/app/app.module.ts
+++ routing/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DepartmentListComponent } from './department-list/department-list.component';
+import { EmployeesListComponent } from './employees-list/employees-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DepartmentListComponent,
+    EmployeesListComponent
   ],
   imports: [
     BrowserModule,
